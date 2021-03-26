f22118f0d2586a90033821921ee7a8ff8f64b7c5
Francisco Manuel Ortuño <curroortuno@gmail.com>
2021-03-26 12:56:14 +0100

    add consensus rebuild

--- docker_files/fixFASTA.py
+++ docker_files/fixFASTA.py
@@ -8,10 +8,14 @@ def main():
     parser = argparse.ArgumentParser(description = 'Remove - from reference in alignment')
     parser.add_argument('--fasta', metavar='fasta', help = "Alignment FASTA", required = True)
     parser.add_argument('--output', metavar="output", help = "FASTA without - in reference", required = True)
+    parser.add_argument('--mode', metavar="mode", help = "Select mode 'fix' or 'join'", default="fasta", required = False)
 
     args = parser.parse_args()
 
-    fix_fasta(args.fasta, args.output)
+    if args.mode == "fasta":
+      fix_fasta(args.fasta, args.output)
+    elif args.mode == "consensus":
+      fix_consensus(args.fasta, args.output)      
 
 def fix_fasta(fasta_name, output):
 
@@ -44,6 +48,55 @@ def fix_fasta(fasta_name, output):
         for key in fasta_dict:
            outfasta.write(key + '\n')
            outfasta.write(fasta_dict[key] + '\n')
+
+
+def fix_consensus(fasta_name, output):
+
+    # Read multi-fasta file and keep sequences
+    fh = open(fasta_name)
+    sequences = ["", ""]
+    seqid = -1
+    for line in fh:
+        if line[0] == ">":
+           seqid += 1
+           if seqid == 1:
+              header = line.strip()
+        else:
+           sequences[seqid] = sequences[seqid] + line.strip()
+
+    # Decide from where keep nucleotides
+    consensus = ""
+    init_gap = False
+    end_gap = ""
+    normalNT = ["A", "T", "G", "C", "-"]
+    for pos in range(0, len(sequences[1])):
+        
+        # If normal nucleotides
+        if sequences[0][pos] in normalNT:
+            if sequences[0][pos] == "-":
+              if init_gap == False:
+                  consensus = consensus + sequences[1][pos]
+              else:
+                  end_gap = end_gap + sequences[1][pos]
+            else:
+              init_gap = True
+              end_gap = ""
+              consensus = consensus + sequences[0][pos]
+
+        # If unknown or ambiguous, get nucleotide from imputation
+        else:
+            if sequences[1][pos] != "-":
+              init_gap = True
+              end_gap = ""
+              consensus = consensus + sequences[1][pos]
+
+    consensus = consensus + end_gap
+
+    # Save fixed consensus 
+    with open(output, 'w') as outfasta:
+        outfasta.write(header + '\n')
+        outfasta.write(consensus + '\n')
+
 
 if __name__ == '__main__':
     main()
